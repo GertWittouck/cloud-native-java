d2edcb9e5f21b68806ca694511e9af1510a844b4
Gert Wittouck <gertwittouck@gmail.com>
2020-04-19 20:34:29 +0100

    chapter-2-spring-guide start

---
+++ chapter-2-spring-guide/src/main/java/com/example/chapter2springguide/Chapter2SpringGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.chapter2springguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter2SpringGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter2SpringGuideApplication.class, args);
+	}
+
+}
---
+++ chapter-2-spring-guide/src/test/java/com/example/chapter2springguide/Chapter2SpringGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter2springguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2SpringGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
